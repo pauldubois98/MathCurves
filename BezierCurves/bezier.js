061a75fae1b4e09dab3a33e6f9df3bcaeac6fe84
pauldubois98 <pauldubois98@gmail.com>
2022-11-13 19:58:26 +0100

    draw lines functions

--- BezierCurves/bezier.js
+++ BezierCurves/bezier.js
@@ -2,6 +2,7 @@ var canvas = document.getElementById("bezier-canvas");
 const WIDTH = canvas.clientWidth;
 const HEIGHT = canvas.clientHeight;
 var n = range_pts.value;
+var t = 0;
 var xs = [];
 var ys = [];
 var ctx = canvas.getContext("2d");
@@ -34,4 +35,40 @@ function bin(n, k) {
   var den = 1;
   for (var i = 2; i <= n - k; i++) den = den * i;
   return num / den;
+}
+
+function intermediates(l, t) {
+  new_l = [];
+  for (let i = 1; i < l.length; i++) {
+    const p1 = l[i - 1];
+    const p2 = l[i];
+    new_l.push((1 - t) * p1 + p2 * t);
+  }
+  return new_l;
+}
+
+function draw_inter_pts(t) {
+  var xs_copy = xs;
+  var ys_copy = ys;
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  ctx.fillStyle = "black";
+  while (xs_copy.length > 0) {
+    for (let i = 0; i < xs_copy.length; i++) {
+      const x = xs_copy[i];
+      const y = ys_copy[i];
+      ctx.beginPath();
+      ctx.arc(x, y, 3, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+    ctx.beginPath();
+    ctx.moveTo(xs_copy[0], ys_copy[0]);
+    for (let i = 1; i < xs_copy.length; i++) {
+      const x = xs_copy[i];
+      const y = ys_copy[i];
+      ctx.lineTo(x, y);
+    }
+    ctx.stroke();
+    xs_copy = intermediates(xs_copy, t);
+    ys_copy = intermediates(ys_copy, t);
+  }
 }
