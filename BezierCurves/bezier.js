0edb74f78d05267c693a5bdfdd487186cf5804bb
pauldubois98 <pauldubois98@gmail.com>
2022-11-13 18:25:09 +0100

    draw points function

--- BezierCurves/bezier.js
+++ BezierCurves/bezier.js
@@ -2,3 +2,19 @@ var canvas = document.getElementById("bezier-canvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "green";
 ctx.fillRect(10, 10, 100, 100);
+
+function draw(n) {
+  xs = [];
+  ys = [];
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  ctx.fillStyle = "grey";
+  for (let i = 0; i < n; i++) {
+    var x = (WIDTH * (i + 0.5)) / n;
+    var y = (HEIGHT * (1 + (i % 2))) / 3;
+    xs.push(x);
+    ys.push(y);
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
