a32fbdea09f5ea7fc290dcc00d292043e1741675
pauldubois98 <pauldubois98@gmail.com>
2024-01-08 09:03:10 +0100

    moving pts

--- FourierCurves/fourier.js
+++ FourierCurves/fourier.js
@@ -17,9 +17,50 @@ function render() {
   ctx.fillStyle = "black";
   for (let i = 1; i < PTS.length; i++) {
     ctx.beginPath();
-    ctx.arc(PTS[i].x, PTS[i].y, 3, 0, 2 * Math.PI);
+    ctx.arc(PTS[i].x, PTS[i].y, 4, 0, 2 * Math.PI);
     ctx.fill();
     ctx.closePath();
+    if (mouse_down && i == mouse_point) {
+      ctx.strokeStyle = "grey";
+      ctx.lineWidth = 2;
+      ctx.beginPath();
+      ctx.arc(PTS[i].x, PTS[i].y, 6, 0, 2 * Math.PI);
+      ctx.stroke();
+      ctx.closePath();
+    }
   }
 }
 render();
+
+function get_mouse_pos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top,
+  };
+}
+canvas.addEventListener("pointerdown", function (evt) {
+  var mousePos = get_mouse_pos(canvas, evt);
+  mouse_down = false;
+  for (let i = 0; i < PTS.length; i++) {
+    if ((PTS[i].x - mousePos.x) ** 2 + (PTS[i].y - mousePos.y) ** 2 < 100) {
+      mouse_point = i;
+      mouse_down = true;
+    }
+  }
+  render();
+});
+
+canvas.addEventListener("pointermove", function (evt) {
+  var mousePos = get_mouse_pos(canvas, evt);
+  if (mouse_down) {
+    PTS[mouse_point] = mousePos;
+  }
+  render();
+});
+
+canvas.addEventListener("pointerup", function (evt) {
+  mouse_down = false;
+  prev_mousePos = undefined;
+  render();
+});
