89a70fb716b3317f7c6ef4117d7202fce7ddd6b6
pauldubois98 <pauldubois98@gmail.com>
2024-05-09 19:53:58 +0200

    drawing global vars

--- PolynomialCurves/main.js
+++ PolynomialCurves/main.js
@@ -1,5 +1,7 @@
 var xs = [];
 var ys = [];
+var ctx = canvas.getContext("2d");
+var POINTER_DOWN = false;
 
 function map(value, start1, stop1, start2, stop2) {
   return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
